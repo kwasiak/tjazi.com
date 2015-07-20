de21d5b71f55f59bb4af3738bf3bf6e73ef55367
Krzysztof Wasiak <k.wasiak@gmail.com>
2015-07-20 15:35:15 +0100

    Adding error message when chatroom already exists

--- webapp/src/main/javascript/angularcontroller/newChatroomWithUserController.js
+++ webapp/src/main/javascript/angularcontroller/newChatroomWithUserController.js
@@ -11,12 +11,31 @@
     function chatroomWithUserController($chatroomService, $scope) {
 
         /* jshint validthis: true */
-
-        $scope.chatroomName = "";
-        $scope.administratorUserName = "";
+        initControllerModels();
 
         $scope.onContinueButtonClick = function() {
             $chatroomService.isChatroomExist($scope.chatroomName);
+
+            $scope.chatroomAlreadyExistsError = true;
         };
+
+        // make sure we catch the close event
+        // this is jQuery call
+        $('#dlgUserChatName').on('hidden.bs.modal', function() {
+            resetForm();
+        });
+
+        function initControllerModels() {
+            $scope.chatroomName = "";
+            $scope.administratorUserName = "";
+            $scope.chatroomAlreadyExistsError = false;
+        }
+
+        function resetForm() {
+            initControllerModels();
+
+            // without this line form won't be updated
+            $scope.$apply();
+        }
     }
 }());
