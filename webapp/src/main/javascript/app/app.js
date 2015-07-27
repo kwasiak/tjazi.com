e191c7455532bc9d6bd658b0619be43657bece04
Krzysztof Wasiak <k.wasiak@gmail.com>
2015-07-27 09:20:22 +0100

    Adding first draft of the login controller

--- webapp/src/main/javascript/app/app.js
+++ webapp/src/main/javascript/app/app.js
@@ -24,6 +24,19 @@
                             templateUrl: templatesSecureRoot + "/chatScreen.html",
                             controller: "ChatScreenController as vm",
                             params: { chatroomUuid: null }
+                        })
+                        .state("loginScreen", {
+                            url: "/login",
+                            templateUrl: templatesRoot + "/loginScreen.html",
+                            controller: "LoginController as vm",
+                            params:
+                            {
+                                // state, which has to be called after log-in is completed
+                                "callbackStateName": null,
+
+                                // parameters, which have to be sent to the callbackStateName
+                                "callbackStateParams": null
+                            }
                         });
 
                     // make sure all requests will be decorated with the proper header
