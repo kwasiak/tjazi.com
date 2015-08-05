7349901a7e838b853e3cc2919160b4e6c45add8d
Krzysztof Wasiak <k.wasiak@gmail.com>
2015-08-05 09:18:31 +0100

    Finished: joining user to the existing chat

--- webapp/src/main/java/com/tjazi/webapp/controller/chatroom/ChatroomJoinController.java
+++ webapp/src/main/java/com/tjazi/webapp/controller/chatroom/ChatroomJoinController.java
@@ -1,7 +1,11 @@
 package com.tjazi.webapp.controller.chatroom;
 
+import com.tjazi.chatroom.service.ChatroomService;
+import com.tjazi.chatroom.service.SingleChatroomDriver;
+import com.tjazi.security.service.SecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,26 +20,51 @@ import java.util.UUID;
 @RequestMapping(value = "/chatroom")
 public class ChatroomJoinController {
 
+    @Autowired
+    private ChatroomService chatroomService;
+
+    @Autowired
+    private SecurityService securityService;
+
     private static final Logger log = LoggerFactory.getLogger(ChatroomJoinController.class);
 
     @RequestMapping(value = "/join/{chatroomUuidStr}", method = RequestMethod.GET)
-    public String joinChatroom(@PathVariable String chatoomUuidStr) {
+    public String joinChatroom(@PathVariable String chatroomUuidStr) {
 
-        if (chatoomUuidStr == null || chatoomUuidStr.isEmpty()){
+        if (chatroomUuidStr == null || chatroomUuidStr.isEmpty()){
             log.error("chatoomUuidStr is null or empty. Navigating to /");
-            return "redirect:/";
+            return getHomeRedirectionUrl();
         }
 
-        UUID chatroomUuid = this.parseChatroomUuid(chatoomUuidStr);
+        UUID chatroomUuid = this.parseChatroomUuid(chatroomUuidStr);
 
         if (chatroomUuid == null) {
+            /* TODO: add error reporting at the client side */
+            // cut down the bad UUID to 100 characters to avoid cases of
+            // someone trying to push a lot of content into log file
             log.error("Problems with parsing entry chatroomUuidStr ('{}'). Navigating to /",
-                    chatoomUuidStr.substring(0, 100));
-            return "redirect:/";
+                    chatroomUuidStr.substring(0, chatroomUuidStr.length() < 100 ? chatroomUuidStr.length() : 100));
+            return getHomeRedirectionUrl();
         }
 
+        SingleChatroomDriver chatroomByUuid = chatroomService.findChatroomByUuid(chatroomUuid);
 
-        return "redirect:/";
+        if (chatroomByUuid == null) {
+            /* TODO: add error reporting at the client side */
+            log.error("Can't find Chatroom with given UUID ('{}'). Navigating to /", chatroomByUuid);
+            return getHomeRedirectionUrl();
+        }
+
+        String currentUser = securityService.getCurrentUserName();
+
+        if (chatroomByUuid.isUserInChatroom(currentUser)) {
+            log.warn("User '{}' is already part of chatroom '{}' (UUID: {}).", currentUser, chatroomByUuid.getChatroomName(), chatroomUuid);
+            return getChatroomRedirectionUrl(chatroomUuid);
+        }
+
+        chatroomByUuid.addUserToChatroom(currentUser);
+
+        return getChatroomRedirectionUrl(chatroomUuid);
     }
 
     private UUID parseChatroomUuid(String chatroomUuid) {
@@ -44,8 +73,18 @@ public class ChatroomJoinController {
             return UUID.fromString(chatroomUuid);
         }
         catch(IllegalArgumentException ex){
+
+            // the UUID string is clearly broken
             return null;
         }
+    }
+
+    private String getHomeRedirectionUrl() {
+        return "redirect:/";
+    }
+
+    private String getChatroomRedirectionUrl(UUID chatroomUuid) {
+        return "redirect:/index#chatscreen/" + chatroomUuid;
     }
 
 }
